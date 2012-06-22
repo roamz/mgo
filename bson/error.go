80e348fc5f29f9a4d983ded5292c10218a6cef92
Gustavo Niemeyer <gustavo@niemeyer.net>
2012-06-22 16:37:16 -0300

    Added error yielding files so people move their code to the new import locations.

---
+++ bson/error.go
@@ -0,0 +1,5 @@
+package error
+
+func error() {
+	`Import labix.org/v1/mgo/bson or labix.org/v2/mgo/bson instead.`
+}
---
+++ error.go
@@ -0,0 +1,5 @@
+package error
+
+func error() {
+	`Import labix.org/v1/mgo or labix.org/v2/mgo instead.`
+}
