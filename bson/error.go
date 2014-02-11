3a2d4fc7cad7b9902c69f1c40c240be5097c9df5
Peter DeGroff <peter@roamz.com>
2014-02-11 16:52:13 +1100

    dropped old error files from bad import

--- bson/error.go
+++
@@ -1,5 +0,0 @@
-package error
-
-func error() {
-	`Import labix.org/v2/mgo/bson instead (or v1, if you haven't updated).`
-}
--- error.go
+++
@@ -1,5 +0,0 @@
-package error
-
-func error() {
-	`Import labix.org/v2/mgo instead (or v1, if you haven't updated).`
-}
